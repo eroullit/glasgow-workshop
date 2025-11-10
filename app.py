408cc63a2d8029b4af5e37c7bd38bfa710684f66
Emmanuel Roullit <eroullit@github.com>
2025-11-10 22:42:40 +0100

    feat: add tree preservation orders
    Signed-off-by: Emmanuel Roullit <eroullit@github.com>

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 import folium
+import json
 
 app = Flask(__name__)
 
@@ -21,6 +22,36 @@ def show_glasgow_map():
         popup='Glasgow City Centre',
         tooltip='Glasgow'
     ).add_to(glasgow_map)
+    
+    # Load and add Tree Preservation Orders
+    with open('Tree_Preservation_Orders_(TPOs).geojson', 'r') as f:
+        tpo_data = json.load(f)
+    
+    # Filter out features with null geometries
+    tpo_data['features'] = [
+        feature for feature in tpo_data['features'] 
+        if feature.get('geometry') is not None
+    ]
+    
+    # Add TPO layer to the map
+    folium.GeoJson(
+        tpo_data,
+        name='Tree Preservation Orders',
+        style_function=lambda feature: {
+            'fillColor': '#228B22',
+            'color': '#006400',
+            'weight': 2,
+            'fillOpacity': 0.4
+        },
+        tooltip=folium.GeoJsonTooltip(
+            fields=['AREANAME', 'EVNUMBER'],
+            aliases=['Area Name:', 'EV Number:'],
+            localize=True
+        )
+    ).add_to(glasgow_map)
+    
+    # Add layer control
+    folium.LayerControl().add_to(glasgow_map)
     
     # Return the map as HTML
     return glasgow_map._repr_html_()
